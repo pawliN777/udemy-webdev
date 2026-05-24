58b0945327221f1c329841856b9a9961060e43d1
pawliN777 <paulobren17@gmail.com>
2026-05-24 10:49:04 -0300

    refactor(toggleTheme): ajustar código, melhorando identação e nomesde variáveis

--- pipastyle/js/theme/theme.js
+++ pipastyle/js/theme/theme.js
@@ -1,25 +1,27 @@
 const storageKey = 'pipastyle-theme';
 
 const applyTheme = (theme) => {
-    if (theme === 'dark') {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-    }
-    localStorage.setItem(storageKey, theme);
+  if (theme === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+  localStorage.setItem(storageKey, theme);
 };
 
 const savedTheme = localStorage.getItem(storageKey);
 if (savedTheme) {
-    applyTheme(savedTheme);
+  applyTheme(savedTheme);
 } else {
-    const systemIsDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    applyTheme(systemIsDark ? 'dark' : 'light');
+  const systemIsDark = window.matchMedia(
+    '(prefers-color-scheme: dark)',
+  ).matches;
+  applyTheme(systemIsDark ? 'dark' : 'light');
 }
 
 export const toggleTheme = () => {
-    const currentTheme = document.documentElement.getAttribute('data-theme');
-    
-    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-    applyTheme(newTheme);
+  const currentTheme = document.documentElement.getAttribute('data-theme');
+
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  applyTheme(newTheme);
 };
--- pipastyle/js/theme/themeButton.js
+++ pipastyle/js/theme/themeButton.js
@@ -1,19 +1,20 @@
 import { toggleTheme } from './theme.js';
-  
-const btn = document.querySelector('#theme-toggle');
-    
-if (btn) {
-    btn.addEventListener('click', toggleTheme);
+
+const ToggleThemeButton = document.querySelector('#theme-toggle');
+
+if (ToggleThemeButton) {
+  ToggleThemeButton.addEventListener('click', toggleTheme);
 }
 
 /* 
 
 <div class="card shadow-lg p-2 mb-5 rounded-full">
-    <button
-        id="theme-toggle" 
-        class="btn btn-secondary square-md btn-icon-circle rounded-full">
-        <i class="icon icon-sun-moon text-2xl"></i>
-     </button>
+  <button
+    id="theme-toggle" 
+      class="btn btn-secondary square-md btn-icon-circle rounded-full"
+      >
+    <i class="icon icon-sun-moon text-2xl"></i>
+  </button>
 </div> 
 
 */
