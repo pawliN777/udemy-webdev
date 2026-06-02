2a268f53996ecef77020db7c56d432cf6cb3f101
pawliN777 <paulobren17@gmail.com>
2026-06-01 22:05:52 -0300

    feat(aula63): adicionar aula nova

---
+++ aulas/aula63/main.js
@@ -0,0 +1,8 @@
+// splice(índice, delete, elmento1, elemento2, elemento3);
+
+const names = ['Paulo', 'Brendhon', 'Henriques', 'Oliveira'];
+
+const removedNames = names.splice(-2, 0, 'Saturno');
+
+
+console.log(names, removedNames)
