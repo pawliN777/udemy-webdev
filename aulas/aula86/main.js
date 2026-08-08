280bbb90d57d0dbfdcc348ebc9c60e92f4d37202
pawliN777 <paulobren17@gmail.com>
2026-08-07 22:05:16 -0300

    feat(aula86): adicionar aula nova

--- aulas/aula86/main.js
+++ aulas/aula86/main.js
@@ -1,7 +1,34 @@
-function waitThere(msg, time) {
-  setTimeout(() => {
-    console.log(msg);
-  }, time);
+function randomTime(min, max) {
+  min *= 1000;
+  max *= 1000;
+
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
-waitThere('Olá', 1000);
+function waitThere(msg, time, cb) {
+  return new Promise((resolve, reject) => {
+    if (typeof msg !== 'string') reject('O valor não é uma string.');
+
+    setTimeout(() => {
+      resolve(msg);
+    }, time);
+  });
+}
+
+waitThere('Um', randomTime(1, 3))
+  .then((answer) => {
+    console.log(answer);
+    return waitThere(2, randomTime(1, 3));
+  })
+  .then((answer) => {
+    console.log(answer);
+    return waitThere('Três', randomTime(1, 3));
+  })
+  .then((answer) => {
+    console.log(answer);
+  })
+  .catch((e) => {
+    console.log('ERRO:', e);
+  });
+
+console.log('Isso será exibido antes de qualquer promise.');
