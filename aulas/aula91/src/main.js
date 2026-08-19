132d9539b860b92e0236df6fed035f909e4fe332
pawliN777 <paulobren17@gmail.com>
2026-08-18 22:37:50 -0300

    feat(aula91): adicionar aula nova

---
+++ aulas/aula91/src/main.js
@@ -0,0 +1,31 @@
+axios('users.json')
+  .then(response => loadElements(response.data));
+
+// fetch('users.json')
+//   .then(response => response.json())
+//   .then(json => loadElements(json));
+
+  const loadElements = (json) => {
+    const table = document.createElement('table');
+
+    for (let user of json) {
+      const tr = document.createElement('tr');
+
+      let td1 = document.createElement('td');
+      td1.innerHTML = user.nome;
+      tr.appendChild(td1);
+
+      let td2 = document.createElement('td');
+      td2.innerHTML = user.idade;
+      tr.appendChild(td2);
+
+      let td3 = document.createElement('td');
+      td3.innerHTML = user.salario;
+      tr.appendChild(td3);
+
+      table.appendChild(tr);
+    }
+
+    const result = document.querySelector('#result');
+    result.appendChild(table);
+  }
