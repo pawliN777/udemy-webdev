81d231196720f100c957665db0d457e67587f36a
pawliN777 <paulobren17@gmail.com>
2026-09-14 19:27:31 -0300

    feat(aula104): adicionar aula nova

---
+++ aulas/aula104/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+          
+// CRUD -> CREATE, READ, UPDATE, DELETE
+//         POST    GET   PUT     DELETE
+
+// http://meusite.com/ <- GET -> Entregar a página
+// http://meusite.com/about <- GET -> Entregar a página sobre
+// http://meusite.com/contact <- GET -> Entregar a página de contatos
+
+app.get('/', (req, res) => {
+  res.send(`
+    <form action="/" method="POST">
+      Nome: <input type="text" name="nome">
+      <button>Enviar form</button>
+    </form>
+    `);
+});
+
+app.post('/', (req, res) => {
+  res.send('Formulário enviado!');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('Obrigado por entrar em contato!');
+});
+
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor executando na porta 3000');
+});
